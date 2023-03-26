0217483b6c205bae5c9bfc4273bf5482e2ab514a
Benedikt Magnus <magnus@magnuscraft.de>
2023-03-26 21:54:51 +0200

    Consistently switched to spaces.

--- addons/MonoCustomResourceRegistry/RegisteredTypeAttribute.cs
+++ addons/MonoCustomResourceRegistry/RegisteredTypeAttribute.cs
@@ -5,14 +5,14 @@ namespace MonoCustomResourceRegistry;
 [AttributeUsage(AttributeTargets.Class)]
 public class RegisteredTypeAttribute : Attribute
 {
-	public string name;
-	public string iconPath;
-	public string baseType;
+    public string name;
+    public string iconPath;
+    public string baseType;
 
-	public RegisteredTypeAttribute(string name, string iconPath = "", string baseType = "")
-	{
-		this.name = name;
-		this.iconPath = iconPath;
-		this.baseType = baseType;
-	}
+    public RegisteredTypeAttribute(string name, string iconPath = "", string baseType = "")
+    {
+        this.name = name;
+        this.iconPath = iconPath;
+        this.baseType = baseType;
+    }
 }
